e91a48da89d881726f84d83768ace92714ef56bc
Reggie Gilliard <regg19970@gmail.com>
2023-03-27 18:16:25 -0400

    add code/files needed to continue work on swirl exercises

---
+++ swirl_course.R
@@ -0,0 +1,11 @@
+# Set path
+setwd("D:/Documents/Work/DegreesNYC/Degrees_data")
+
+# Load packages
+pkgs <- c("swirlify")
+lapply(pkgs, library, character.only = T)
+
+# Lesson 1
+new_lesson("Lesson 1", "Exercises")
+
+set_lesson()
