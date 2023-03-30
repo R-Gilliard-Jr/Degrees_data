b9242dab13032793e8a694a20f6112e968d7d6de
Reggie Gilliard <regg19970@gmail.com>
2023-03-29 22:13:16 -0400

    updates to swirl course and course data

--- swirl_course.R
+++ swirl_course.R
@@ -1,11 +1,12 @@
 # Set path
-setwd("D:/Documents/Work/DegreesNYC/Degrees_data")
+# setwd("D:/Documents/Work/DegreesNYC/Degrees_data")
+setwd("D:/Documents/DegreesNYC/Degrees_data")
 
 # Load packages
 pkgs <- c("swirlify")
 lapply(pkgs, library, character.only = T)
 
 # Lesson 1
-new_lesson("Lesson 1", "Exercises")
+new_lesson("Lesson 3", "Exercises")
 
 set_lesson()
